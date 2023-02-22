3345f38c096015d2983ea191d6cf33d5954bc03b
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 09:36:51 +0600

    probleme of mixed line fixed

--- templet.js
+++ templet.js
@@ -1,6 +1,6 @@
 const single='rohim \n and \n korim';
 const num1=20;
 const num2=30;
-const sum='num1 is'+ num1 +'num2 is'+num2;
+const sum='num1 is '+ num1 +' num2 is '+num2;
 console.log(single);
 console.log(sum);
