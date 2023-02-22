1f3d7a33b6bf20f0bdb93513257012e9e18b85e1
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 10:40:27 +0600

    single line single  parameter arrow function  simplify version

--- more-arrow.js
+++ more-arrow.js
@@ -4,6 +4,7 @@ const add1 =(a, b, c, d)=> a+b+c+d;
 
 //single line single parameter arrow function
 const add2 =(num)=>num*2;
-
-const result=add2(5);
+// single line single parameter arrow function simplify version
+const add3=number=>number*3;
+const result=add3(5);
 console.log(result);
