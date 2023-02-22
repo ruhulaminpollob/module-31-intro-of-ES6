dc9f428f1039b550ebebb95e68d612af93e2383c
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 10:20:33 +0600

    arrow function

--- arrow-function.js
+++ arrow-function.js
@@ -1,10 +1,12 @@
+//  regular declare 
  function add(num1, num2) {
     return num1+num2;
  }
-
+// regular anonymous function
  const add2=function (num1, num2) {
     return num1+num2;
  }
-
- const total=add2(10,12);
+// arrow function
+ const add3=(num1, num2)=>num1+num2;
+ const total=add3(10,12);
  console.log(total);
