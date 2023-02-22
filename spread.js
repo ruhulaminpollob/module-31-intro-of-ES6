e309bd477b647e07e82c6a82014c0181b778774c
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 14:07:17 +0600

    added a number in a arrayy

--- spread.js
+++ spread.js
@@ -1,5 +1,10 @@
 const max=Math.max(1,3,4,5,22,2);
-console.log(max);
+// console.log(max);
 const numbers=[1,3,4,6,2,9,22,12];
 const largest=Math.max(...numbers);
-console.log(...numbers);
+// console.log(largest);
+const numbers2=numbers;
+numbers.push(55)
+numbers2.push(77)
+console.log(numbers);
+console.log(numbers2);
