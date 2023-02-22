88ba8ceeb28dde00b8a31bec4aa5b4f27c15819e
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 14:29:53 +0600

    practice destructuring object element

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+// destructuring object
+
+const fish={
+    name:'puti',
+    color:'silver',
+    age:5,
+}
+// const name=fish.name;
+// console.log(name);
+
+const {name,age}=fish;
+console.log(age);
