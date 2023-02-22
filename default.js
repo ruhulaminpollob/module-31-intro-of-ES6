2d5ceee78ee8eeb41f80e2fb2e2c51fefcd131df
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 09:02:44 +0600

    lets explore es6

---
+++ default.js
@@ -0,0 +1,9 @@
+// function add(first, second) {
+//     if(second===undefined){
+//         second=0;
+//     }
+//     const sum=first+second;
+//     return sum;
+// }
+// console.log(add(12));
+
