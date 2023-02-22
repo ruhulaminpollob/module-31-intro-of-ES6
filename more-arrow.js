f0b8c855a4914a6a389c5f4f4a661cdfb1c96e5a
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-02-22 10:43:49 +0600

    multiline arrow function added

--- more-arrow.js
+++ more-arrow.js
@@ -6,5 +6,11 @@ const add1 =(a, b, c, d)=> a+b+c+d;
 const add2 =(num)=>num*2;
 // single line single parameter arrow function simplify version
 const add3=number=>number*3;
+// multiline arrow function
+const add4=(num1,num2,num3)=>{
+    const sum=num1+num2+num3;
+    const multiply=sum*2;
+    return multiply;
+}
 const result=add3(5);
 console.log(result);
